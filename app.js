abf955688fecd9ff90cceb5d3003265a124e5d74
VEBata <vkhloponya@yandex.ru>
2023-09-03 23:01:06 +0300

    rev

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log("TEST")
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const router = require('./routes/index');
+
+const app = express();
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+}).then(() => {
+  console.log('Подключен к БД');
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64dc89507c21f858b16cf435',
+  };
+
+  next();
+});
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Приложение запущено, порт: ${PORT}`);
+});
